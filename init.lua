b7c2b4c92fd24ce2f6c38dfc654c4e5cee5e7395
AndresGR02 <andres.gr.workspace@gmail.com>
2024-03-19 13:50:48 -0600

    Add new plugin to telescope

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+---@diagnostic disable-next-line: unused-local
 local opts = {}
 
 require("lazy").setup("plugins")
